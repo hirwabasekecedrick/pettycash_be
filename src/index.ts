4095942801c72dc92fdeb904346a7089e8db28b9
hirwabasekecedrick <hirwabasekecedrick@gmail.com>
2026-06-16 09:20:43 +0300

    adding the origin acceptance

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const limiter = rateLimit({
 });
 app.use('/api/', limiter);
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
