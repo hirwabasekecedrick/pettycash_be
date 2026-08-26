ef64ba2ceff2bf2162da90b7418cd5d57eb9ee12
hirwabasekecedrick <hirwabasekecedrick@gmail.com>
2026-08-26 16:03:02 +0200

    Adding working code

--- dist/utils/prisma.js
+++ dist/utils/prisma.js
@@ -3,6 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("../generated/prisma/client");
 const adapter_pg_1 = require("@prisma/adapter-pg");
 require("dotenv/config");
-const adapter = new adapter_pg_1.PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new adapter_pg_1.PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
 const prisma = new client_1.PrismaClient({ adapter });
 exports.default = prisma;
--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -2,7 +2,10 @@ import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import 'dotenv/config';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+  ssl: { rejectUnauthorized: false }
+});
 const prisma = new PrismaClient({ adapter } as any);
 
 export default prisma;
